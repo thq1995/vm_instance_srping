671c38f626e9a9a785e50017c7f204a300820694
Tuan Quang <anhtuanquang2201@gmail.com>
2022-05-03 11:57:22 -0700

    first commit

---
+++ src/main/java/com/example/vm_instance_srping/VmInstanceSrpingApplication.java
@@ -0,0 +1,12 @@
+package com.example.vm_instance_srping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmInstanceSrpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmInstanceSrpingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/vm_instance_srping/VmInstanceSrpingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.vm_instance_srping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmInstanceSrpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
